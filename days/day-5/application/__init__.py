424cdf4eb2ed98017486f921f3b7b559812282cd
BenHesketh21 <heskethben0@gmail.com>
2020-09-24 13:02:44 +0000

    Update __init__.py

--- days/day-5/application/__init__.py
+++ days/day-5/application/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI') 
 app.config['SECRET_KEY'] = "my-secret"
 
 db = SQLAlchemy(app)
